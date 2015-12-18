6b53aa24b57eef9387d19d55df2b8c9d8b18f80a
rbagni <rbagni@motorola.com>
2015-12-18 10:09:51 -0200

    Alterando botão de convidar

--- EasyFestas/app/src/main/java/com/pluzone/easyfestas/easyfestas/ListProductActivity.java
+++ EasyFestas/app/src/main/java/com/pluzone/easyfestas/easyfestas/ListProductActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -95,6 +97,14 @@ public class ListProductActivity extends AppCompatActivity {
 //            }
 //
 //        });
+
+        final Button button = (Button) findViewById(R.id.button_convidar);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+            }
+        });
+
     }
 
     public String geraCorpoEmail() {
